ab8d283369f04217b4c3bd45df643f18095eca48
aditisharma6202 <111963350+aditisharma6202@users.noreply.github.com>
2023-04-02 15:26:44 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Courses from './courses/courses';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    
+ 
   </React.StrictMode>
 );
 
